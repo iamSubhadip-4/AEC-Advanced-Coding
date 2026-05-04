0e9faf0a0c6738388bce89fa819a4a28127da46b
Subhadip Maji <subhadipmaji.cse@gmail.com>
2026-05-04 23:23:01 +0530

    Added solutions for Problem 1 and Problem 2

---
+++ arrayTransformation.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[100];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    for (int i = 1; i < n; i++) {
+        if ((arr[i] - arr[0]) % k != 0) {
+            printf("-1\n");
+            return 0;
+        }
+    }
+
+    qsort(arr, n, sizeof(int), compare);
+
+    int median = arr[n / 2];
+
+    int total = 0;
+
+    for (int i = 0; i < n; i++) {
+        int diff = arr[i] - median;
+
+        if (diff < 0)
+            diff = -diff;
+
+        total += diff / k;
+    }
+
+    printf("%d\n", total);
+
+    return 0;
+}
---
+++ cyclicSubstring.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char s[100];
+    scanf("%s",s);
+
+    int n=strlen(s);
+
+    char t[100];
+    strcpy(t,s);
+    strcat(t,s);
+
+    int maxSum=0;
+
+    for (int i=0;i<n;i++) {
+        int visited[26]={0};
+        int sum=0;
+
+        for (int j=i;j<i+n;j++) {
+            int index = t[j]-'a';
+
+            if (visited[index]==1)
+                break;
+
+            visited[index]=1;
+            sum+=(index + 1);
+        }
+
+        if (sum>maxSum)
+            maxSum=sum;
+    }
+
+    printf("%d\n",maxSum);
+
+    return 0;
+}
